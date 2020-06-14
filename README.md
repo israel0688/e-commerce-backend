f7dda6064d96525e9cff6d6e8a2c9dc7e7efe5d2
Israel Berlanga <israel0688@hotmail.com>
2020-06-14 16:43:49 -0500

    updated readme with video and github link

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# E-commerce Back End Starter Code
+# E-commerce Back End
+
+### About
+    * A back end for an e-commerce site that takes a working Express.js API and is configured to use Sequelize to interact with a MYSQL database.
+
+
+### Walkthrough Video
+
+[Link to Walkthrough Video](https://drive.google.com/file/d/1KnQzM_SgeSkOywcjIrz72MOhhq-OjAer/view)
+
+### GitHub Repository 
+
+[Link to Github Repository](https://github.com/israel0688/e-commerce-backend)
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -69,8 +69,13 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
-    .then((product) => {
+ Product.create({
+  product_name: req.body.product_name,
+  price: req.body.price,
+  stock: req.body.stock,
+  category_id: req.body.category_id
+})
+   /* .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
@@ -83,7 +88,7 @@ router.post('/', (req, res) => {
       }
       // if no product tags, just respond
       res.status(200).json(product);
-    })
+    })*/
     .then((productTagIds) => res.status(200).json(productTagIds))
     .catch((err) => {
       console.log(err);
